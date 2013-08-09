86772dd3e357f4249f8fe19a7153c975327a6d37
Dr. Tristan Allwood <tora@doc.ic.ac.uk>
2013-08-09 14:34:09 +0100

    Now with 100% more type checking.

--- Directed.hs
+++ Directed.hs
@@ -28,8 +28,26 @@ type SizeLeft = Int8
 
 type Input  = Word64
 type Output = Word64
+type Value  = Word64
+type Target = Word64
 
-
+data Constraints
+  = Constraints
+  { allowedOp1s :: [Op1]
+  , allowedOp2s :: [Op2]
+  , size :: Int8
+  , hasFold :: Bool
+  , hasTFold :: Bool
+  }
 
 search :: PartialProgram -> Input -> Output -> Constraints -> [PartialProgram]
 search = error "TODO"
+
+searchExpression :: PartialExpression -> [Value] -> Target
+                 -> Constraints -> SizeLeft -> [PartialExpression]
+searchExpression Unforced vals target constraints sizeleft                  = [] {- TODO -}
+searchExpression (Terminal terminals) vals target constraints sizeleft      = []
+searchExpression (If0 c ift iff) vals target constraints sizeleft           = []
+searchExpression (Fold over init function) vals target constraints sizeleft = []
+searchExpression (Op1 ops exp) vals target constraints sizeleft             = []
+searchExpression (Op2 ops lhs rhs) vals target constraints sizeleft         = []
